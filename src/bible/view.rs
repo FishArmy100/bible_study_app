9cd5e7bc6964151e68d4a504f05997eb9acc8294
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2024-03-15 02:49:34 -0400

    Bible text showing

--- src/bible/view.rs
+++ src/bible/view.rs
@@ -1,13 +1,13 @@
 use std::sync::Arc;
-use eframe::egui;
+use eframe::egui::{self, ScrollArea};
 
-use super::Bible;
+use super::{Bible, Chapter};
 
 pub struct BiblePanel
 {
     bible: Arc<Bible>,
-    book: u32,
-    chapter: u32,
+    book_index: u32,
+    chapter_index: u32,
 }
 
 impl BiblePanel
@@ -17,39 +17,54 @@ impl BiblePanel
         Self 
         {
             bible,
-            book: 0,
-            chapter: 0,
+            book_index: 0,
+            chapter_index: 0,
         }
     }
 
     pub fn update(&mut self, ui: &mut egui::Ui)
     {
         egui::Grid::new("book_selector").show(ui, |ui| {
-            let book_label = &self.bible.books[self.book as usize].name;
+            let book_label = &self.bible.books[self.book_index as usize].name;
             egui::ComboBox::from_id_source("book_selector")
                 .selected_text(book_label)
                 .show_ui(ui, |ui| {
                     let mut index = 0;
                     for b in &self.bible.books
                     {
-                        ui.selectable_value(&mut self.book, index, &b.name);
+                        ui.selectable_value(&mut self.book_index, index, &b.name);
                         index += 1;
                     }
                 });
 
-            let chapter_number = &self.bible.books[self.book as usize].chapters[self.chapter as usize].number;
-            let chapter_label = format!("Chapter {}", chapter_number);
+            let chapter = self.chapter();
+            let chapter_label = format!("Chapter {}", chapter.number);
             egui::ComboBox::from_id_source("chapter_selector")
                 .selected_text(chapter_label)
                 .show_ui(ui, |ui| {
-                    let chapter_count = self.bible.books[self.book as usize].chapters.len() as u32;
+                    let chapter_count = self.bible.books[self.book_index as usize].chapters.len() as u32;
                     for i in 0..chapter_count
                     {
-                        ui.selectable_value(&mut self.chapter, i, &format!("Chapter {}", i + 1));
+                        ui.selectable_value(&mut self.chapter_index, i, &format!("Chapter {}", i + 1));
                     }
                 })
         });
 
         ui.separator();
+
+        ScrollArea::vertical().id_source("verses_area").show(ui, |ui| {
+            for v in &self.chapter().verses
+            {
+                egui::Grid::new(v.number).show(ui, |ui| {
+                    ui.label(v.number.to_string());
+                    ui.label(&v.text);
+                });
+            }
+        });
+    }
+
+    fn chapter(&self) -> &Chapter
+    {
+        &self.bible.books[self.book_index as usize].chapters[self.chapter_index as usize]
     }
 }
