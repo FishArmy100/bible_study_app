7ccea56acd342bf756263f7d0c07b9c873bffdd2
FishArmy100 <87653948+FishArmy100@users.noreply.github.com>
2024-03-15 02:29:30 -0400

    chapter selection working

--- src/bible/view.rs
+++ src/bible/view.rs
@@ -25,22 +25,31 @@ impl BiblePanel
     pub fn update(&mut self, ui: &mut egui::Ui)
     {
         egui::Grid::new("book_selector").show(ui, |ui| {
-            egui::ComboBox::from_label("Select a Book:").show_ui(ui, |ui| {
-                let mut index = 0;
-                for b in &self.bible.books
-                {
-                    ui.selectable_value(&mut self.book, index, &b.name);
-                    index += 1;
-                }
-            });
+            let book_label = &self.bible.books[self.book as usize].name;
+            egui::ComboBox::from_id_source("book_selector")
+                .selected_text(book_label)
+                .show_ui(ui, |ui| {
+                    let mut index = 0;
+                    for b in &self.bible.books
+                    {
+                        ui.selectable_value(&mut self.book, index, &b.name);
+                        index += 1;
+                    }
+                });
 
-            egui::ComboBox::from_label("Select a Chapter:").show_ui(ui, |ui| {
-                let chapter_count = self.bible.books[self.book as usize].chapters.len() as u32;
-                for i in 0..chapter_count
-                {
-                    ui.selectable_value(&mut self.chapter, i, &format!("Chapter {}", i + 1));
-                }
-            })
+            let chapter_number = &self.bible.books[self.book as usize].chapters[self.chapter as usize].number;
+            let chapter_label = format!("Chapter {}", chapter_number);
+            egui::ComboBox::from_id_source("chapter_selector")
+                .selected_text(chapter_label)
+                .show_ui(ui, |ui| {
+                    let chapter_count = self.bible.books[self.book as usize].chapters.len() as u32;
+                    for i in 0..chapter_count
+                    {
+                        ui.selectable_value(&mut self.chapter, i, &format!("Chapter {}", i + 1));
+                    }
+                })
         });
+
+        ui.separator();
     }
 }
